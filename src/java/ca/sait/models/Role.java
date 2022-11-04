1bff4759100d549bf6323ec0d4099021175c2369
BuolaA <113210923+BuolaA@users.noreply.github.com>
2022-11-04 10:03:12 -0600

    Models created
    User and roles classes created and defined.

--- src/java/ca/sait/models/Role.java
+++ src/java/ca/sait/models/Role.java
@@ -1,15 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ca.sait.models;
 
 import java.io.Serializable;
 
 /**
- *
- * @author veryn
+ *Represents a Role
+ * @author Buola Achor
  */
 public class Role implements Serializable {
+    private int id;
+    private String name;
     
+    public Role() {
+        
+    }
+    
+    public Role(int id, String name) {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
